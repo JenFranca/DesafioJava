8631dd65c1d5418dd57b2901255916b221ea4c95
JenFranca <jenifferfretesfranca@gmail.com>
2025-08-19 18:34:05 -0300

    Amadurecendo ideia da primeira atividade

--- Main.java
+++ Main.java
@@ -4,21 +4,32 @@ public class Main
 {
     public static void main(String[] args) 
     {
-        System.out.println("Eleitores!");
+        System.out.println("Eleição!");
 
-        int eleitores;  
-        int votosValidos;
-        int votosBrancos;
-        int votosNulos;
+        int eleitores = 1000;  
+        int votosValidos = 800;
+        int votosBrancos = 150;
+        int votosNulos = 50;
 
-        eleitores = 1000;
-        votosValidos = 800;
-        votosBrancos = 150;
-        votosNulos = 50;
-
+         
+        System.out.println("");
+        System.out.println("Quantidade de eleitores:");
         System.out.println("Número de eleitores: " + eleitores);
+
+        System.out.println("");
+        System.out.println("Quantidade de votos:");
         System.out.println("Número de votos válidos: " + votosValidos);
         System.out.println("Número de votos em branco: " + votosBrancos);
         System.out.println("Número de votos nulos: " + votosNulos);  
+
+        double percentualVotosValidos = (double) votosValidos / eleitores * 100;
+        double percentualVotosBrancos = (double) votosBrancos / eleitores * 100;
+        double percentualVotosNulos = (double) votosNulos / eleitores * 100;
+
+        System.out.println("");
+        System.out.println("Em porcentagem:");
+        System.out.println("Percentual de votos válidos: " + percentualVotosValidos + "%");
+        System.out.println("Percentual de votos em branco: " + percentualVotosBrancos + "%");
+        System.out.println("Percentual de votos nulos: " + percentualVotosNulos + "%");
     }
 }
