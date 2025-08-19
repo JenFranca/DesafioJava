22e175598a965d5a81bae5d0efcbf96760964e10
JenFranca <jenifferfretesfranca@gmail.com>
2025-08-19 17:47:17 -0300

    Testando primeiros passos em java

---
+++ Main.java
@@ -0,0 +1,11 @@
+///usr/bin/env jbang "$0" "$@" ; exit $?
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Olá, mundo!");
+
+        int eleitores;            
+        eleitores = 100;          
+        System.out.println("Número de eleitores: " + eleitores);  
+    }
+}
