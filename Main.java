6c12c401450a630a5f5448525262a1729fa1b375
JenFranca <jenifferfretesfranca@gmail.com>
2025-08-19 17:57:24 -0300

    Inserindo valores

--- Main.java
+++ Main.java
@@ -1,11 +1,24 @@
 ///usr/bin/env jbang "$0" "$@" ; exit $?
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Olá, mundo!");
+public class Main 
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("Eleitores!");
 
-        int eleitores;            
-        eleitores = 100;          
-        System.out.println("Número de eleitores: " + eleitores);  
+        int eleitores;  
+        int votosValidos;
+        int votosBrancos;
+        int votosNulos;
+
+        eleitores = 1000;
+        votosValidos = 800;
+        votosBrancos = 150;
+        votosNulos = 50;
+
+        System.out.println("Número de eleitores: " + eleitores);
+        System.out.println("Número de votos válidos: " + votosValidos);
+        System.out.println("Número de votos em branco: " + votosBrancos);
+        System.out.println("Número de votos nulos: " + votosNulos);  
     }
 }
