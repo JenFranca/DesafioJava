7748162e1c1bfc6f8d6c24dc69317e0c51cec173
JenFranca <jenifferfretesfranca@gmail.com>
2025-08-19 22:05:20 -0300

    Criando menu

--- Main.java
+++ Main.java
@@ -1,10 +1,37 @@
 ///usr/bin/env jbang "$0" "$@" ; exit $?
 
+import java.util.Scanner;
+
 public class Main 
 {
     public static void main(String[] args) {
-        Votos.main(args);
+        Scanner scanner = new Scanner(System.in);
+        int opcao;
+
+        do {
+            System.out.println("\nMenu:");
+            System.out.println("1 - Eleição");
+            System.out.println("2 - BubbleSort");
+            System.out.println("0 - Sair");
+            System.out.print("Escolha uma opção: ");
+
+            opcao = scanner.nextInt();
+
+            switch (opcao) {
+                case 1:
+                    Votos.main(args);
+                    break;
+                case 2:
+                    BubbleSort.bubbleSort(args);
+                    break;
+                case 0:
+                    System.out.println("Saindo...");
+                    break;
+                default:
+                    System.out.println("Opção inválida.");
+            }
+        } while (opcao != 0);
+
+        scanner.close();
     }
 }
-
-
