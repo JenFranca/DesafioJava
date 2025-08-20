d89198b78d0622398f3a570dd4876aa02b362083
JenFranca <jenifferfretesfranca@gmail.com>
2025-08-20 17:55:38 -0300

    Começando atividade do fatorial

---
+++ Fatorial.java
@@ -0,0 +1,13 @@
+
+import java.util.Scanner;
+
+public class Fatorial {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in); // cria um scanner
+
+        System.out.print("Digite um número: ");
+        int numero = scanner.nextInt(); // lê o número
+
+        scanner.close(); 
+    }
+}
