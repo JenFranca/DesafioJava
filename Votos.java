b29185d226173b0209e53c302848c507c2b8a9b2
JenFranca <jenifferfretesfranca@gmail.com>
2025-08-19 21:24:03 -0300

    Finalizando Votos e separando classe main

---
+++ Votos.java
@@ -0,0 +1,52 @@
+public class Votos 
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("Eleição!");
+
+        int eleitores = 1000;  
+        int votosValidos = 800;
+        int votosBrancos = 150;
+        int votosNulos = 50;
+
+        System.out.println("");
+        System.out.println("Quantidade de eleitores:");
+        System.out.println("Número de eleitores: " + eleitores);
+
+        System.out.println("");
+        System.out.println("Quantidade de votos:");
+        System.out.println("Número de votos válidos: " + votosValidos);
+        System.out.println("Número de votos em branco: " + votosBrancos);
+        System.out.println("Número de votos nulos: " + votosNulos);  
+
+        PercentualVotosEleicao calculadora = new PercentualVotosEleicao(); 
+
+        double percentualVotosValidos = calculadora.calcularPercentualValidos(votosValidos, eleitores);
+        double percentualVotosBrancos = calculadora.calcularPercentualBrancos(votosBrancos, eleitores);
+        double percentualVotosNulos = calculadora.calcularPercentualNulos(votosNulos, eleitores);
+
+        System.out.println("");
+        System.out.println("Em porcentagem:");
+        System.out.println("Percentual de votos válidos: " + percentualVotosValidos + "%");
+        System.out.println("Percentual de votos em branco: " + percentualVotosBrancos + "%");
+        System.out.println("Percentual de votos nulos: " + percentualVotosNulos + "%");
+    }
+
+    static class PercentualVotosEleicao 
+    {
+        public double calcularPercentualValidos(int votosValidos, int totalEleitores) 
+        {
+            return (double) votosValidos / totalEleitores * 100;
+        }
+
+        public double calcularPercentualBrancos(int votosBrancos, int totalEleitores)
+        {
+            return (double) votosBrancos / totalEleitores * 100;
+        }
+
+        public double calcularPercentualNulos(int votosNulos, int totalEleitores) 
+        {
+            return (double) votosNulos / totalEleitores * 100;
+        }
+    }
+}
